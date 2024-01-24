3fa931dc67429d7ea71a86e647576843e8832e36
Joseph Replin <joe.replin@kineo.com>
2024-01-23 19:02:02 -0600

    Update bookmarkingResume.jsx

--- templates/bookmarkingResume.jsx
+++ templates/bookmarkingResume.jsx
@@ -1,15 +1,21 @@
 import React from 'react';
+import { classes } from 'core/js/reactHelpers';
 
 export default function Bookmarking(props) {
   const {
     label,
-    ariaLabel
+    ariaLabel,
+    isDisabled
   } = props;
   return (
     <div className="bookmarking-resume__inner">
       <button
         type='button'
-        className='btn-text bookmarking-resume__button js-bookmarking-resume-button'
+        className={classes([
+          'btn-text bookmarking-resume__button js-bookmarking-resume-button',
+          isDisabled
+        ])}
+        aria-disabled={isDisabled || null}
         aria-label={ariaLabel} >
         {label}
       </button>
