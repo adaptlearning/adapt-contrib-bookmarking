9badd562cdb5d4b4ff86b04dcafab4cbc5243430
Chris Steele <chris.steele@kineo.com>
2025-01-27 17:31:23 +0000

    Corrections

--- migrations/v2.js
+++ migrations/v2.js
@@ -1,13 +1,16 @@
 import { describe, whereContent, whereFromPlugin, mutateContent, checkContent, updatePlugin } from 'adapt-migrations';
 
-describe('Bookmarking - v2.0.0 to v2.1.1', async () => {
+describe('Bookmarking - v2.1.1 to v2.1.2', async () => {
+
+  // https://github.com/adaptlearning/adapt-contrib-bookmarking/compare/v2.1.1..v2.1.2
+
   let bookmarkingCfg;
   const defaultBody = 'Bookmarking';
 
-  whereFromPlugin('Bookmarking - from v2.0.0', { name: 'adapt-contrib-bookmarking', version: '>=2.0.0 <2.1.2' });
+  whereFromPlugin('Bookmarking - from v2.1.1', { name: 'adapt-contrib-bookmarking', version: '<2.1.2' });
   whereContent('Bookmarking - where config is present', async (content) => {
     const [course] = content.filter(({ _type }) => _type === 'course');
-    bookmarkingCfg = course._bookmarking
+    bookmarkingCfg = course._bookmarking;
     if (bookmarkingCfg) return true;
   });
 
@@ -24,22 +27,26 @@ describe('Bookmarking - v2.0.0 to v2.1.1', async () => {
   updatePlugin('Bookmarking - update to v2.1.2', { name: 'adapt-contrib-bookmarking', version: '2.1.2', framework: '>=2.2.0' });
 });
 
-describe('Bookmarking - v2.1.2 to v3.0.0', async () => {
+describe('Bookmarking - v2.1.2 to v2.1.3', async () => {
+
+  // https://github.com/adaptlearning/adapt-contrib-bookmarking/compare/v2.1.2..v2.1.3
+
   let bookmarkingCfg;
-  whereFromPlugin('Bookmarking - from v2.1.2', { name: 'adapt-contrib-bookmarking', version: '<3.0.0' });
+  whereFromPlugin('Bookmarking - from v2.1.2', { name: 'adapt-contrib-bookmarking', version: '<2.1.3' });
   whereContent('Bookmarking - where config is present', async (content) => {
     const [course] = content.filter(({ _type }) => _type === 'course');
-    bookmarkingCfg = course._bookmarking
+    bookmarkingCfg = course._bookmarking;
     if (bookmarkingCfg) return true;
   });
 
   mutateContent('Bookmarking - add _showPrompt', async (content) => {
     bookmarkingCfg._showPrompt = true;
+    return true;
   });
 
   checkContent('Bookmarking - check _showPrompt', async (content) => {
     return bookmarkingCfg._showPrompt === true;
   });
 
-  updatePlugin('Bookmarking - update to v3.0.0', { name: 'adapt-contrib-bookmarking', version: '3.0.0', framework: '>=5' });
+  updatePlugin('Bookmarking - update to v2.1.3', { name: 'adapt-contrib-bookmarking', version: '2.1.3', framework: '>=2.2.0' });
 });
--- migrations/v4.js
+++ migrations/v4.js
@@ -1,13 +1,22 @@
 import { describe, whereContent, whereFromPlugin, mutateContent, checkContent, updatePlugin } from 'adapt-migrations';
 
-describe('Bookmarking - v3.0.0 to v4.3.2', async () => {
+describe('Bookmarking - v4.2.1 to v4.3.0', async () => {
+
+  const getCourse = content => {
+    const [course] = content.filter(({ _type }) => _type === 'course');
+    return course;
+  };
+
+  const getGlobals = content => {
+    return getCourse(content)?._globals?._extensions?._bookmarking;
+  };
 
   let course, bookmarkingCfg, courseBookmarkingGlobals;
   const resumeButtonText = 'Resume';
   const resumeButtonAriaLabel = 'Navigate to your furthest point of progress.';
   const location = 'previous';
 
-  whereFromPlugin('Bookmarking - from v3.0.0', { name: 'adapt-contrib-bookmarking', version: '<4.3.2' });
+  whereFromPlugin('Bookmarking - from v4.2.1', { name: 'adapt-contrib-bookmarking', version: '<4.3.0' });
   whereContent('Bookmarking - where config is present', async (content) => {
     [course] = content.filter(({ _type }) => _type === 'course');
     bookmarkingCfg = course._bookmarking;
@@ -15,7 +24,8 @@ describe('Bookmarking - v3.0.0 to v4.3.2', async () => {
   });
 
   mutateContent('Bookmarking - add globals if missing', async (content) => {
-    if (course?._globals?._extensions?._bookmarking) return true;
+    courseBookmarkingGlobals = getGlobals(content);
+    if (courseBookmarkingGlobals) return true;
     course._globals._extensions = course._globals._extensions || {};
     courseBookmarkingGlobals = course._globals._extensions._bookmarking = {};
     return true;
@@ -27,9 +37,11 @@ describe('Bookmarking - v3.0.0 to v4.3.2', async () => {
   });
   mutateContent('Bookmarking - add _location', async (content) => {
     bookmarkingCfg._location = location;
+    return true;
   });
   mutateContent('Bookmarking - add _autoRestore', async (content) => {
     bookmarkingCfg._autoRestore = true;
+    return true;
   });
 
   checkContent('Bookmarking - check new globals', async (content) => {
@@ -45,5 +57,5 @@ describe('Bookmarking - v3.0.0 to v4.3.2', async () => {
     return bookmarkingCfg._autoRestore === true;
   });
 
-  updatePlugin('Bookmarking - update to v4.3.2', { name: 'adapt-contrib-bookmarking', version: '4.3.2', framework: '>=5.31.11' });
+  updatePlugin('Bookmarking - update to v4.3.0', { name: 'adapt-contrib-bookmarking', version: '4.3.0', framework: '>=5.31.11' });
 });
