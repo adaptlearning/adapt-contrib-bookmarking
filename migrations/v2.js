b77386153b3bb9b1abb66f2450c463d52b186a07
Chris Steele <chris.steele@kineo.com>
2025-02-03 10:05:16 +0000

    Migration script consistency

--- migrations/v2.js
+++ migrations/v2.js
@@ -1,27 +1,32 @@
 import { describe, whereContent, whereFromPlugin, mutateContent, checkContent, updatePlugin } from 'adapt-migrations';
 
+const getCourse = content => {
+  const [course] = content.filter(({ _type }) => _type === 'course');
+  return course;
+};
+
 describe('Bookmarking - v2.1.1 to v2.1.2', async () => {
 
   // https://github.com/adaptlearning/adapt-contrib-bookmarking/compare/v2.1.1..v2.1.2
 
-  let bookmarkingCfg;
+  let course;
   const defaultBody = 'Bookmarking';
 
   whereFromPlugin('Bookmarking - from v2.1.1', { name: 'adapt-contrib-bookmarking', version: '<2.1.2' });
+
   whereContent('Bookmarking - where config is present', async (content) => {
-    const [course] = content.filter(({ _type }) => _type === 'course');
-    bookmarkingCfg = course._bookmarking;
-    if (bookmarkingCfg) return true;
+    course = getCourse(content);
+    return course._bookmarking;
   });
 
   mutateContent('Bookmarking - update default body text', async (content) => {
-    if (bookmarkingCfg.body !== defaultBody) return true;
-    bookmarkingCfg.body = 'Would you like to continue where you left off?';
+    if (course._bookmarking.body !== defaultBody) return true;
+    course._bookmarking.body = 'Would you like to continue where you left off?';
     return true;
   });
 
   checkContent('Bookmarking - check body text', async (content) => {
-    return bookmarkingCfg.body !== defaultBody;
+    return course._bookmarking.body !== defaultBody;
   });
 
   updatePlugin('Bookmarking - update to v2.1.2', { name: 'adapt-contrib-bookmarking', version: '2.1.2', framework: '>=2.2.0' });
@@ -31,21 +36,22 @@ describe('Bookmarking - v2.1.2 to v2.1.3', async () => {
 
   // https://github.com/adaptlearning/adapt-contrib-bookmarking/compare/v2.1.2..v2.1.3
 
-  let bookmarkingCfg;
+  let course;
+
   whereFromPlugin('Bookmarking - from v2.1.2', { name: 'adapt-contrib-bookmarking', version: '<2.1.3' });
+
   whereContent('Bookmarking - where config is present', async (content) => {
-    const [course] = content.filter(({ _type }) => _type === 'course');
-    bookmarkingCfg = course._bookmarking;
-    if (bookmarkingCfg) return true;
+    course = getCourse(content);
+    return course._bookmarking;
   });
 
   mutateContent('Bookmarking - add _showPrompt', async (content) => {
-    bookmarkingCfg._showPrompt = true;
+    course._bookmarking._showPrompt = true;
     return true;
   });
 
   checkContent('Bookmarking - check _showPrompt', async (content) => {
-    return bookmarkingCfg._showPrompt === true;
+    return course._bookmarking._showPrompt === true;
   });
 
   updatePlugin('Bookmarking - update to v2.1.3', { name: 'adapt-contrib-bookmarking', version: '2.1.3', framework: '>=2.2.0' });
--- migrations/v4.js
+++ migrations/v4.js
@@ -1,28 +1,28 @@
 import { describe, whereContent, whereFromPlugin, mutateContent, checkContent, updatePlugin } from 'adapt-migrations';
 
+const getCourse = content => {
+  const [course] = content.filter(({ _type }) => _type === 'course');
+  return course;
+};
+
+const getGlobals = content => {
+  return getCourse(content)?._globals?._extensions?._bookmarking;
+};
+
 describe('Bookmarking - v4.2.1 to v4.3.0', async () => {
 
   // https://github.com/adaptlearning/adapt-contrib-bookmarking/compare/v4.2.1..v4.3.0
 
-  const getCourse = content => {
-    const [course] = content.filter(({ _type }) => _type === 'course');
-    return course;
-  };
-
-  const getGlobals = content => {
-    return getCourse(content)?._globals?._extensions?._bookmarking;
-  };
-
-  let course, bookmarkingCfg, courseBookmarkingGlobals;
+  let course, courseBookmarkingGlobals;
   const resumeButtonText = 'Resume';
   const resumeButtonAriaLabel = 'Navigate to your furthest point of progress.';
   const location = 'previous';
 
   whereFromPlugin('Bookmarking - from v4.2.1', { name: 'adapt-contrib-bookmarking', version: '<4.3.0' });
+
   whereContent('Bookmarking - where config is present', async (content) => {
-    [course] = content.filter(({ _type }) => _type === 'course');
-    bookmarkingCfg = course._bookmarking;
-    if (bookmarkingCfg) return true;
+    course = getCourse(content);
+    return course._bookmarking;
   });
 
   mutateContent('Bookmarking - add globals if missing', async (content) => {
@@ -38,11 +38,11 @@ describe('Bookmarking - v4.2.1 to v4.3.0', async () => {
     return true;
   });
   mutateContent('Bookmarking - add _location', async (content) => {
-    bookmarkingCfg._location = location;
+    course._bookmarking._location = location;
     return true;
   });
   mutateContent('Bookmarking - add _autoRestore', async (content) => {
-    bookmarkingCfg._autoRestore = true;
+    course._bookmarking._autoRestore = true;
     return true;
   });
 
@@ -53,10 +53,10 @@ describe('Bookmarking - v4.2.1 to v4.3.0', async () => {
     );
   });
   checkContent('Bookmarking - check _location', async (content) => {
-    return bookmarkingCfg._location === location;
+    return course._bookmarking._location === location;
   });
   checkContent('Bookmarking - check _autoRestore', async (content) => {
-    return bookmarkingCfg._autoRestore === true;
+    return course._bookmarking._autoRestore === true;
   });
 
   updatePlugin('Bookmarking - update to v4.3.0', { name: 'adapt-contrib-bookmarking', version: '4.3.0', framework: '>=5.31.11' });
